5cf98f398cd4690653772a01b504c2cc423237f3
johannajonsson <jojo.risin@outlook.com>
2026-01-08 16:58:55 +0100

    removed friends list in user for now

--- src/main/java/se/jensen/johanna/socialapp/model/User.java
+++ src/main/java/se/jensen/johanna/socialapp/model/User.java
@@ -45,5 +45,5 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
 
-    private List<User> friends = new ArrayList<>();
+   
 }
